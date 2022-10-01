fc60a3d9d2a348bdad1b4d2f45899e8371a13a3a
TAKUYA34 <110417799+TAKUYA34@users.noreply.github.com>
2022-10-01 16:54:30 +0900

    未ログイン時の表示の変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:show, :edit, :delete]
+  before_action :authenticate_user!, only: [:new, :edit, :delete]
   
   def index
     @prototypes = Prototype.all
